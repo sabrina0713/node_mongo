3294ed073459bfba14f94dc6776111d0dc68e8da
Liping Marlow <limarlow@microsoft.com>
2018-11-07 13:56:55 -0600

    first deployment

---
+++ index.js
@@ -0,0 +1,175 @@
+var http = require('http');
+var express = require('express')
+var app = express();
+const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+var mysql = require('mysql');
+var manytimes = 0;
+const bodyParser = require('body-parser');
+app.get('/pooling', function (req, res) {
+    res.send('pooling');
+    //mlabconnection();
+    httprequest();
+  })
+  app.get('/once', function (req, res) {
+    res.send('once')
+    //connectFirstTime();
+}) 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var port = process.env.PORT || 8080;
+app.listen(port);
+
+function httprequest()
+{
+var request = require("request");
+
+var options = { method: 'GET',
+  url: 'http://syndy-exports-test.azurewebsites.net/api/Export',
+  qs: 
+   { sortField: 'RetailerName',
+     sortDirection: 'Ascending',
+     pageNumber: '1',
+     pageSize: '20',
+     userOrganisationId: '00000000-0000-0000-0000-000000000000',
+     cultureId: 'nl' },
+  headers: 
+   { 'postman-token': '92ed0c85-24e8-25f1-9117-694ef62e7fa2',
+     'cache-control': 'no-cache' } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
+}
+function mlabconnection()
+{
+    let mongoDB = 'mongodb://limarlow:lipi5961@ds041167.mlab.com:41167/lptestmongodb';
+    // Use connect method to connect to the server
+    MongoClient.connect(mongoDB, function (err, client) {
+        //assert.equal(null, err);
+        console.log("Connected successfully to server");
+
+        const db = client.db('lptestmongodb');
+        insertDocuments(db, function () {
+            client.close();
+        });
+        client.close();
+    });
+    
+
+
+    
+}
+const insertDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+        { a: 1 }, { a: 2 }, { a: 3 }
+    ], function (err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+}
+function poolingConnection()
+{
+    console.log("inside poolingconnection")
+   // console.log('pooling2: ', mysql._allConnections.length, ', Queued Connections: ', mysql._connectionQueue.length, ', Free Connections: ', mysql._freeConnections.length);
+    var pool = mysql.createPool({ connectionLimit : 10, host: "limarlow.mysql.database.azure.com", user: "limarlow@limarlow", password: "F0rtest0nly", database: "limarlowtest", port: 3306 });
+    pool.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+      
+        // Use the connection
+        connection.query('SELECT * FROM inventory', function (error, results, fields) {
+          // When done with the connection, release it.
+         
+      
+          // Handle error after the release.
+          if (error) throw error;
+      
+          // Don't use the connection here, it has been returned to the pool.
+        })
+        connection.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['banana', 150],
+        function (err, results, fields) {
+            //connection.release();
+            if (err) throw err;
+            else console.log('Inserted ' + results.affectedRows + ' row(s).');
+            
+        })
+        connection.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['orange', 154],
+        function (err, results, fields) {
+            if (err) throw err;
+            console.log('Inserted ' + results.affectedRows + ' row(s).');
+            //connection.release();
+        })
+       connection.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['apple', 100],
+        function (err, results, fields) {
+            if (err) throw err;
+            console.log('Inserted ' + results.affectedRows + ' row(s).');
+            //connection.release();
+        })
+        //console.log('poolinginside: ', mysql._allConnections.length, ', Queued Connections: ', mysql._connectionQueue.length, ', Free Connections: ', mysql._freeConnections.length);
+        connection.release();
+      });
+       /*mysql.on('connection', function (connection) {
+        manytimes=manytimes+1;
+        console.log("called" + manytimes+ + "times");
+        var max_conn = 100;
+        // -- checking connections used length by percentage to trigger manual kill threads
+        
+          console.log('pooling2: ', mysql._allConnections.length, ', Queued Connections: ', mysql._connectionQueue.length, ', Free Connections: ', mysql._freeConnections.length);
+          //killStaleThreads();
+        
+      }); */
+}
+function connectFirstTime() {
+    console.log("inside connectFirstTime")
+    var conn = mysql.createConnection({ host: "limarlow.mysql.database.azure.com", user: "limarlow@limarlow", password: "F0rtest0nly", database: "limarlowtest", port: 3306 });
+
+    conn.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+        queryDatabase(conn);
+
+    });
+    function queryDatabase(conn) {
+        conn.query('DROP TABLE IF EXISTS inventory;', function (err, results, fields) {
+            if (err) throw err;
+            console.log('Dropped inventory table if existed.');
+        })
+        conn.query('CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);',
+            function (err, results, fields) {
+                if (err) throw err;
+                console.log('Created inventory table.');
+            })
+        conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['banana', 150],
+            function (err, results, fields) {
+                if (err) throw err;
+                else console.log('Inserted ' + results.affectedRows + ' row(s).');
+            })
+        conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['orange', 154],
+            function (err, results, fields) {
+                if (err) throw err;
+                console.log('Inserted ' + results.affectedRows + ' row(s).');
+            })
+        conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['apple', 100],
+            function (err, results, fields) {
+                if (err) throw err;
+                console.log('Inserted ' + results.affectedRows + ' row(s).');
+            })
+        conn.end(function (err) {
+            if (err) throw err;
+            else console.log('Done.')
+        });
+    };
+};
+    console.log("Server running at http://localhost:%d", port);
+
